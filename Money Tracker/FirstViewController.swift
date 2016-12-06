85173cb0b8b1f9145feac6f90c1c40904cfdb6b7
RKrueger <student@rkrueger.local>
2016-12-06 09:17:27 -0600

    Initial Commit

---
+++ Money Tracker/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Money Tracker
+//
+//  Created by RKrueger on 12/6/16.
+//  Copyright © 2016 RKrueger. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
